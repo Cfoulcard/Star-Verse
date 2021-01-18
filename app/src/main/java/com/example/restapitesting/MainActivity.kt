d1c3ca638fb6c8ca29c8407e9eb1b58e633bfdf0
Christian Foulcard <cfoulcard@gmail.com>
2021-01-18 15:27:39 -0600

    Working Restapi

--- app/src/main/java/com/example/restapitesting/MainActivity.kt
+++ app/src/main/java/com/example/restapitesting/MainActivity.kt
@@ -15,6 +15,7 @@ import com.android.volley.toolbox.JsonArrayRequest
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
+import com.bumptech.glide.Glide
 import org.json.JSONArray
 import org.json.JSONException
 import org.json.JSONObject
@@ -26,33 +27,45 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-    }
-
-    fun clicker(v: View) {
         // Instantiate the RequestQueue.
         val queue = Volley.newRequestQueue(this)
-        val textView = findViewById<TextView>(R.id.text)
+
+        val textView1 = findViewById<TextView>(R.id.astropic)
+        val textView2 = findViewById<TextView>(R.id.text)
         val imageView = findViewById<ImageView>(R.id.image)
 
         // Parse the URL
         val url = "https://api.nasa.gov/planetary/apod/?api_key=tpzRnSgZLfddTEoBGSS1DeLtODnMFFNrKrTbAvEL"
 
-        // Create JSON Object
+        // Create JSON Object using JSONObjectRequest
         val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url, null,
-                { response ->
-                    textView.text = "Response: %s".format(response["explanation"])
-                    val bmpUrl = URL (response.getString("hdurl"))
+            { response ->
+                try {
+                    // Get NASA's title listed in the JSON
+                    textView1.text = "%s".format(response["title"])
+                    // Put NASA's explanation as the text as listed in the JSON
+                    textView2.text = "%s".format(response["explanation"])
 
-                },
-                { error ->
-                    // TODO: Handle error
+                    // Places NASA's image in the imageView
+                    Glide.with(this).load(response.getString("hdurl")).into(imageView)
+                } catch (e: JSONException) {
+
                 }
+            },
+            { error ->
+                Toast.makeText(this, "Cannot get info", Toast.LENGTH_SHORT).show()
+            }
         )
 
         // Use the RequestQueue which is needed to parse the JSON
         queue.add(jsonObjectRequest)
+
     }
-}
+
+    }
+
+
+
 
 
 
